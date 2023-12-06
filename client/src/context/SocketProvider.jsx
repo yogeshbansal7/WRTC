95109ecdba3c695b42109919b0c8e97451cac992
Yogesh Bansal <101383618+yogeshbansal7@users.noreply.github.com>
2023-12-06 13:11:29 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://wrtc-caz4.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
